93dc124711eec09aab06f7a7079ecce37aa631d2
preetham <preetham.yerrakondappa@gmail.com>
2021-01-15 20:36:44 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('DevOps Training!'));
+app.get('/', (req, res) => res.send('DevOps engineer training get started!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
